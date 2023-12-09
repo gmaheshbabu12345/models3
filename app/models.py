81f5155a25df0e5c0c19cf07394d4ea2a9219cba
gmaheshbabu12345 <babu80567@gmail.com.com>
2023-12-09 09:08:27 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Dept(models.Model):
+    deptno=models.IntegerField(primary_key=True)
+    dname=models.CharField(max_length=100)
+    loc=models.CharField(max_length=100)
+class Emp(models.Model):
+    empno=models.IntegerField(primary_key=True)
+    ename=models.CharField(max_length=100)
+    deptno=models.ForeignKey(Dept('deptno'),on_delete=models.CASCADE)
+class SalGrade(models.Model):
+    empno=models.ForeignKey(Emp('empno'),on_delete=models.CASCADE)
+    sal=models.IntegerField()
+    comm=models.IntegerField()
+class Bonus(models.Model):
+    empno=models.ForeignKey(Emp('empno'),on_delete=models.CASCADE)
+    bonus=models.IntegerField()
