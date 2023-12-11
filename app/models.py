00f409b12333086aef1fd8337b689ef0f626c362
gmaheshbabu12345 <babu80567@gmail.com.com>
2023-12-11 21:13:13 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -5,10 +5,12 @@ class Dept(models.Model):
     deptno=models.IntegerField(primary_key=True)
     dname=models.CharField(max_length=100)
     loc=models.CharField(max_length=100)
+
 class Emp(models.Model):
     empno=models.IntegerField(primary_key=True)
     ename=models.CharField(max_length=100)
     deptno=models.ForeignKey(Dept('deptno'),on_delete=models.CASCADE)
+    sal=models.IntegerField(null=True)
 class SalGrade(models.Model):
     empno=models.ForeignKey(Emp('empno'),on_delete=models.CASCADE)
     sal=models.IntegerField()
